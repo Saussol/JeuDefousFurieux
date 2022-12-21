ad8c3ef9f141d1a4fff95c00c27739b9186e9c43
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2022-12-21 10:20:49 +0100

    gather gift with network

--- Assets/Scripts/GiftUse.cs
+++ Assets/Scripts/GiftUse.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Unity.Netcode;
+using Unity.Collections;
 
 public class GiftUse : NetworkBehaviour
 {
     public TextMeshProUGUI textOnGift;
-    public int points;
+    public NetworkVariable<FixedString128Bytes> points = new NetworkVariable<FixedString128Bytes>(
+        "0", NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
 
-    
+    public void Start()
+    {
+        textOnGift.text = points.Value.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyObjectServerRPC();
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    void DestroyObjectServerRPC()
+    {
+        Destroy(gameObject);
+    }
 }
--- Assets/Scripts/SimpleSpawn.cs
+++ Assets/Scripts/SimpleSpawn.cs
@@ -38,24 +38,25 @@ public class SimpleSpawn : NetworkBehaviour
         for (int i = 0; i < gifts.Length; i++)
         {
             GameObject go = Instantiate(gifts[i].box);
-            go.GetComponent<NetworkObject>().Spawn();
+
             go.transform.position = new Vector3(Random.Range(-10.0f, 10.0f), 3f, Random.Range(-10.0f, 10.0f));
             go.transform.localScale = gifts[i].boxScale;
             //Add all gift in gameManager
             GameManager.Instance.giftsInGame.Add(go);
 
             //Collider A for interaction
-            go.AddComponent<BoxCollider>().size = (gifts[i].boxScale * 2);
-            go.GetComponent<BoxCollider>().isTrigger = true;
+            //go.AddComponent<BoxCollider>().size = (gifts[i].boxScale * 2);
+            //go.GetComponent<BoxCollider>().isTrigger = true;
             //Collider B for collide with ground
-            go.AddComponent<BoxCollider>();
+            //go.AddComponent<BoxCollider>();
             //Add & use rigid for fall gift it's cool
             /*go.AddComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ;
             go.GetComponent<Rigidbody>().mass = 0.01f;
             go.GetComponent<Rigidbody>().drag = 10f;*/
 
-            go.GetComponent<GiftUse>().points = gifts[i].points;
-            go.GetComponent<GiftUse>().textOnGift.text = "" + gifts[i].points;
+            go.GetComponent<GiftUse>().points.Value = gifts[i].points.ToString();
+
+            go.GetComponent<NetworkObject>().Spawn();
         }
     }
 }
