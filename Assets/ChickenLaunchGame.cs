1e5c11c18b5ca4d518a3363d54bec9c5b2944c0e
Romain Pitot <60229037+Globulous@users.noreply.github.com>
2022-12-22 10:16:34 +0100

    Anim Menu Réparé

--- Assets/ChickenLaunchGame.cs
+++ Assets/ChickenLaunchGame.cs
@@ -30,20 +30,22 @@ public class ChickenLaunchGame : MonoBehaviour
 
     private void OnMouseDown()
     {
+        m_Animator.SetBool("Etonne", true); // lance poule anim de quand on lui clique dessus
         StartCoroutine(WaitPoule());
     }
 
     IEnumerator WaitPoule()
     {
-        m_Animator.SetBool("Etonne", true);
-        m_Animatorc.SetBool("Gocam", true);
+        
+        yield return new WaitForSeconds(1f);
+        m_Animatorc.SetBool("Gocam", true); // lance cam
         yield return new WaitForSeconds(2.5f);
-        m_Animatorr.SetBool("Walk", true);
+        m_Animatorr.SetBool("Walk", true); // lance poule
         yield return new WaitForSeconds(0.25f);
-        m_Animatorr.SetBool("Run", true);
-        canvasFondu.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
-        m_Animatorfondu.SetBool("LaunchFondu", true);
+        m_Animatorr.SetBool("Run", true); // lance poule qui court
+        canvasFondu.SetActive(true); // active le GO pour le fondu
+        yield return new WaitForSeconds(1f);
+        m_Animatorfondu.SetBool("LaunchFondu", true); // lance fondu
         // launch lobby
     }
 }
