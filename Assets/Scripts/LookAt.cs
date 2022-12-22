7f9d8be33f405d0004c239772153dc3d3c5f4420
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2022-12-22 15:06:51 +0100

    billboard 6

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -8,11 +8,11 @@ public class LookAt : NetworkBehaviour
     public GameObject theCam;
     void Start()
     {
-        FindCamClientRPC();
+        FindCamServerRPC();
     }
 
-    [ClientRpc]
-    private void FindCamClientRPC()
+    [ServerRpc(RequireOwnership = false)]
+    private void FindCamServerRPC()
     {
         theCam = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.GetComponent<PlayerMovement1>().cinemachineFree.gameObject;
     }
@@ -25,15 +25,15 @@ public class LookAt : NetworkBehaviour
     [ClientRpc]
     private void UpdateRotationClientRPC()
     {
-        if(theCam == null)
-        {
-            theCam = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.GetComponent<PlayerMovement1>().cinemachineFree.gameObject;
-            return;
-        }
-        else
-        {
+        //if(theCam == null)
+        //{
+        //    theCam = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.GetComponent<PlayerMovement1>().cinemachineFree.gameObject;
+        //    return;
+        //}
+        //else
+        //{
             transform.LookAt(theCam.transform);
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
-        }
+        //}
     }
 }
