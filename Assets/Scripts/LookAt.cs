b0cafe2e7d6420c95367e5708b80a86960cefb95
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2022-12-22 14:40:00 +0100

    billboard

--- Assets/Scripts/LookAt.cs
+++ Assets/Scripts/LookAt.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class LookAt : MonoBehaviour
+public class LookAt : NetworkBehaviour
 {
-    //public Camera theCam;
+    public GameObject theCam;
     void Start()
     {
-        //theCam = Camera.main;
+        theCam = NetworkManager.Singleton.ConnectedClients[NetworkManager.Singleton.LocalClientId].PlayerObject.transform.GetChild(2).gameObject; ;
     }
 
     void Update()
     {
-       // transform.LookAt(theCam.transform);
+       transform.LookAt(theCam.transform);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + 180, transform.eulerAngles.z);
     }
 }
