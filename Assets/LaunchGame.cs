d77d3eba4ac07d0eaf5991d6e9dec6e419a38e38
Saturn1B <60229050+Saturn1B@users.noreply.github.com>
2022-12-22 09:46:33 +0100

    hide launch button

--- Assets/LaunchGame.cs
+++ Assets/LaunchGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LaunchGame : NetworkBehaviour
 {
