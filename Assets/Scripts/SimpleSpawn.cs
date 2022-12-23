d407d0ce94da05f189287777d746e261be0bfd71
Saussol <60229019+Saussol@users.noreply.github.com>
2022-12-23 09:36:50 +0100

    modif simple spawn

--- Assets/Scripts/SimpleSpawn.cs
+++ Assets/Scripts/SimpleSpawn.cs
@@ -39,7 +39,7 @@ public class SimpleSpawn : NetworkBehaviour
         {
             GameObject go = Instantiate(gifts[i].prefab);
 
-            go.transform.position = new Vector3(Random.Range(30, 90), 30, Random.Range(20, 40));
+            go.transform.position = new Vector3(Random.Range(25, 105), 30, Random.Range(0, 80));
             go.transform.localScale = gifts[i].boxScale;
             go.GetComponent<GiftUse>().gift = gifts[i];
             //GameManager.Instance.giftsInGame.Add(go);
